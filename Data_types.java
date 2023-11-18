e03beed4d510264db80ab2e07332a87a8b2078c9
SomnathKar000 <somnathkar201901@gmail.com>
2023-11-18 18:49:33 +0530

    data_types and variables

---
+++ Data_types.java
@@ -0,0 +1,29 @@
+public class Data_types {
+    public static void main (String args[]){
+        double d=75.36;
+        float f=73.36f;
+
+        int a=10;
+        byte by =122;
+        long l=12456;
+        short sh=1234;
+
+        char c='a';
+        String b="hello";
+        
+        boolean e=true;
+
+        System.out.println(d);
+        System.out.println(f);
+
+        System.out.println(a);
+        System.out.println(by);
+        System.out.println(l);
+        System.out.println(sh);
+        
+        System.out.println(c);
+        System.out.println(b);
+        
+        System.out.println(e);
+    }
+}
