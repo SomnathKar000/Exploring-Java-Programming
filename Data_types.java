a87747826b1075e70e5d03da8a9a7a84bd4bda70
SomnathKar000 <somnathkar201901@gmail.com>
2023-11-19 16:34:09 +0530

    Operators Conditional Statements

--- Data_types.java
+++ Data_types.java
@@ -7,6 +7,7 @@ public class Data_types {
         byte by =122;
         long l=12456;
         short sh=1234;
+        int binary=0b010;
 
         char c='a';
         String b="hello";
@@ -20,10 +21,22 @@ public class Data_types {
         System.out.println(by);
         System.out.println(l);
         System.out.println(sh);
+        System.out.println(binary);
         
         System.out.println(c);
         System.out.println(b);
         
         System.out.println(e);
+
+        byte bi=126;
+        int in=123;
+        in=bi;
+        bi=(byte)in;
+        System.out.println(bi);
+
+        byte t1=10;
+        byte t2=30;
+        int t3=t1*t2;
+        System.out.println(t3);
     }
 }
