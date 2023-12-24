5edb6e184e050f9c6607ef1823a058c7d3565d5f
SomnathKar000 <somnathkar201901@gmail.com>
2023-12-24 21:13:09 +0530

    Anonymous Class and Interface

---
+++ AnonymousInnerClass.java
@@ -0,0 +1,25 @@
+public class AnonymousInnerClass {
+    public static void main(String[] args) {
+        A obj =new A(){
+            public void show(){
+                System.err.println("In new show");
+            }
+        };// Anonymous Inner Class
+        obj.show();
+
+        AbstractClass obj2=new AbstractClass(){
+            public void show(){
+                System.out.println("In new show");
+            }
+        };
+        obj2.show();
+    }
+}
+class A{
+    public void show() {
+        System.out.println("In show");
+    }
+}
+abstract class AbstractClass{
+    abstract void show();
+}
