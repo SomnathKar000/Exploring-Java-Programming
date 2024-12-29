a58b7c018b8c98beaca353700e8cc666a8c1e55f
Somnath Kar <somnathkar201901@gmail.com>
2024-12-29 15:23:58 +0530

    added data types

---
+++ Explore/Hello.java
@@ -0,0 +1,10 @@
+
+class Hello{
+    public static void main(String args[]){
+        int num=23;
+        char name='S';
+        float f=2.3f;
+        boolean b=true;
+        System.out.println("Hello Java"+num+name+f+b);
+    }
+}
