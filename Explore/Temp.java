205a58e6bdabf48aa03f6278b48f785ba696f9e5
Somnath Kar <somnathkar201901@gmail.com>
2025-01-04 19:48:09 +0530

    2d array

--- Explore/Temp.java
+++ Explore/Temp.java
@@ -1,15 +1,15 @@
 public class Temp {
     public static void main(String [] args){
-        // int[] arr=new int[2];
-        int arr[]={2,3};
-        arr[0]=1;
-        for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
+        int multiArr[][]=new int[3][];
+        multiArr[0]=new int[2];
+        multiArr[1]=new int[5];
+        multiArr[2]=new int[3];
+        for(int i=0;i<multiArr.length;i++){
+            for(int j=0;j<multiArr[i].length;j++){
+                System.out.print(multiArr[i][j]);
+            }
+            System.out.println();
         }
-
-    //    for(int n:arr){
-    //     System.err.println(n);
-    //    }
 
     }
 }
