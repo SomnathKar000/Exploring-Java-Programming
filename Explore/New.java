d7c7a103291d7791b5a535d14cf87e08f348923d
Somnath Kar <somnathkar201901@gmail.com>
2025-01-04 18:27:26 +0530

    Loops

--- Explore/New.java
+++ Explore/New.java
@@ -2,5 +2,18 @@ public class New {
     public static void main(String[] args){
         int a=10;
         System.out.println(a%2==0?"Even ":"Odd");
+        for(int i=0;i<a;i++){
+            System.out.println("Current No is :- "+i);
+        }
+        while(a>0){
+            System.out.println("The current no is :- "+a);
+            a--;
+        }
+        do{
+            System.out.println("The current no is :- "+a);
+            a--;
+        }while(a>0);
+
+
     }
 }
