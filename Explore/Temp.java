fbf4a2abf29ed20ba49254fd3add7468d3d29e71
Somnath Kar <somnathkar201901@gmail.com>
2025-01-04 19:38:35 +0530

    Static array

---
+++ Explore/Temp.java
@@ -0,0 +1,15 @@
+public class Temp {
+    public static void main(String [] args){
+        // int[] arr=new int[2];
+        int arr[]={2,3};
+        arr[0]=1;
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
+    //    for(int n:arr){
+    //     System.err.println(n);
+    //    }
+
+    }
+}
