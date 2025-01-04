888934b559f0b0ef4d270d6bf0c294b6b9063539
Somnath Kar <somnathkar201901@gmail.com>
2025-01-04 19:59:20 +0530

    assing and print value in 2d Array

--- Explore/Temp.java
+++ Explore/Temp.java
@@ -4,9 +4,21 @@ public class Temp {
         multiArr[0]=new int[2];
         multiArr[1]=new int[5];
         multiArr[2]=new int[3];
+
+        // for(int i=0;i<multiArr.length;i++){
+        //     for(int j=0;j<multiArr[i].length;j++){
+        //         multiArr[i][j]=(int)(Math.random()*9+1);
+        //     }
+        // }
+        for(int arr[]:multiArr){
+            for(int i=0;i<arr.length;i++){
+                arr[i]=(int)(Math.random()*9+1);
+            }
+        }
+
         for(int i=0;i<multiArr.length;i++){
             for(int j=0;j<multiArr[i].length;j++){
-                System.out.print(multiArr[i][j]);
+                System.out.print(multiArr[i][j]+" ");
             }
             System.out.println();
         }
