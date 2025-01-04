85633bd2c451de491b17f5a0d8c5c61e2ae83a71
Somnath Kar <somnathkar201901@gmail.com>
2025-01-04 18:57:28 +0530

    class

--- Explore/New.java
+++ Explore/New.java
@@ -1,19 +1,27 @@
 public class New {
     public static void main(String[] args){
         int a=10;
-        System.out.println(a%2==0?"Even ":"Odd");
-        for(int i=0;i<a;i++){
-            System.out.println("Current No is :- "+i);
-        }
-        while(a>0){
-            System.out.println("The current no is :- "+a);
-            a--;
-        }
-        do{
-            System.out.println("The current no is :- "+a);
-            a--;
-        }while(a>0);
+        // System.out.println(a%2==0?"Even ":"Odd");
+        // for(int i=0;i<a;i++){
+        //     System.out.println("Current No is :- "+i);
+        // }
+        // while(a>0){
+        //     System.out.println("The current no is :- "+a);
+        //     a--;
+        // }
+        // do{
+        //     System.out.println("The current no is :- "+a);
+        //     a--;
+        // }while(a>0);
+        // Calculator obj= new Calculator();
+        int sum=Calculator.sum(10,20);
+        System.out.println(sum);
 
+    }
+}
 
+class Calculator{
+     static int sum(int num1,int num2){
+        return num1+num2;
     }
 }
