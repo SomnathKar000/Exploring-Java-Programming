79884335238a9e3687289034a9e4d19a97752391
Somnath Kar <somnathkar201901@gmail.com>
2025-01-04 20:05:27 +0530

    Jagged array

--- Explore/Temp.java
+++ Explore/Temp.java
@@ -1,9 +1,10 @@
 public class Temp {
     public static void main(String [] args){
         int multiArr[][]=new int[3][];
-        multiArr[0]=new int[2];
-        multiArr[1]=new int[5];
-        multiArr[2]=new int[3];
+       for(int i=0;i<multiArr.length;i++){
+        int randomNum=(int)(Math.random()*10);
+        multiArr[i]= new int[randomNum];
+       }
 
         // for(int i=0;i<multiArr.length;i++){
         //     for(int j=0;j<multiArr[i].length;j++){
