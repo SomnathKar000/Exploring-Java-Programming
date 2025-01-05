736ad46c5b0b9045d1e94ba980bf9a79d6d9189c
Somnath Kar <somnathkar201901@gmail.com>
2025-01-05 20:39:53 +0530

    Encaplution

--- Explore/Temp.java
+++ Explore/Temp.java
@@ -1,41 +1,18 @@
 public class Temp {
     public static void main(String [] args){
-        Mobile obj1= new Mobile();
-        obj1.price=10000;
-        obj1.brand="Samsung";
-
-        Mobile obj2 = new Mobile();
-        obj2.price=2000;
-        obj2.brand="Nokia";
-
-        obj1.show();
-        obj2.show();
-        Mobile.view();
-        Mobile.show(obj2);
+        Car obj = new Car();
+        obj.setColor("Red");
+        obj.print();
     }
 }
 
-class Mobile{
-   static String name;
-    int price;
-    String brand;
-    static{
-        System.out.println("Static block"); // Called only once
-        name="Mobile";
-    }
-    public Mobile(){
-        System.out.println("Constructor"); // Called every time object is created
-        price=0;
-        brand="NA";
-    }
+class Car{
+    private String color;
 
-    public void show(){
-        System.out.println(name+" : "+brand+" : "+ price );
+    public void print(){
+        System.out.println("Car color is: " + color);
     }
-    public static void view(){
-        System.out.println("Viewing");
-    }
-    public static void show(Mobile obj){
-        System.out.println(name+" : "+obj.brand+" : "+ obj.price );
+    public void setColor(String color){
+        this.color = color;
     }
 }
