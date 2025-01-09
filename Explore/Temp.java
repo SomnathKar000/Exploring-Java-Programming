d43d02a41fbaf04eeecc0f1092366477cbfe5229
Somnath Kar <somnathkar201901@gmail.com>
2025-01-09 19:16:26 +0530

    super method

--- Explore/Temp.java
+++ Explore/Temp.java
@@ -1,7 +1,24 @@
 public class Temp {
     public static void main(String [] args){
-        Calc cal=new AdvCalc();
-        System.out.println(cal.sub(4, 1));
+        B obj = new B(7);
     }
 }
 
+class A{
+    public A(){
+        System.out.println("In A");
+    }
+    public A(int i){
+        System.out.println("In A int");
+    }
+}
+class B extends A{
+    public B(){
+        System.out.println("In B");
+    }
+
+    public B(int i){
+        super(i);
+        System.out.println("In B int");
+    }
+}
