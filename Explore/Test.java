c19c4eeedfcf4b8581c797cdcc0e19ae4401956e
Somnath Kar <somnathkar201901@gmail.com>
2025-01-18 18:07:25 +0530

    Anonymous inner class

--- Explore/Test.java
+++ Explore/Test.java
@@ -1,33 +1,23 @@
 public class Test {
     public static void main(String args[]) {
-        A obj = new A();
-        obj.print();
-        A.B obj2 = obj.new B();
-        obj2.print();
-        A.B.C obj3 = new A.B.C();
-        obj3.print();
+        A obj = new B() {
+            public void show() {
+                System.out.println("In New Show");
+            }
+        };
+        obj.show();
 
     }
 }
 
 class A {
-    int num = 12;
-
-    public void print() {
-        System.out.println("Print A");
+    public void show() {
+        System.out.println("In A Show");
     }
+}
 
-    class B {
-        public void print() {
-            System.out.println("Print B");
-        }
-
-        static class C {
-            public void print() {
-                System.out.println("Print C");
-            }
-        }
-
+class B extends A {
+    public void show() {
+        System.out.println("In B Show");
     }
-
 }
