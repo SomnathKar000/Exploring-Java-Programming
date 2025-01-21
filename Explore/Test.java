1fca73c555156e2c42de252efe235b10248dac81
Somnath Kar <somnathkar201901@gmail.com>
2025-01-21 21:45:45 +0530

    functionnal interface in java

--- Explore/Test.java
+++ Explore/Test.java
@@ -1,26 +1,15 @@
 public class Test {
     public static void main(String args[]) {
-        Laptop obj = Laptop.Windows;
-        System.out.println(obj + " : " + obj.getPrice());
-
+        A obj = new A() {
+            public void show() {
+                System.out.println("This is show");
+            }
+        };
+        obj.show();
     }
 }
 
-enum Laptop {
-    Mac(50000), Windows(60000), Samsung(30000);
-
-    private int price;
-
-    private Laptop(int price) {
-        this.price = price;
-    }
-
-    public int getPrice() {
-        return this.price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
+@FunctionalInterface
+interface A {
+    void show();
 }
