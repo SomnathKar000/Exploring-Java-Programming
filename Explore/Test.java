64dcd8dc13dc7e8f2bad699fae3d5dd54680d33c
Somnath Kar <somnathkar201901@gmail.com>
2025-01-21 21:14:39 +0530

    enum

--- Explore/Test.java
+++ Explore/Test.java
@@ -1,25 +1,12 @@
 public class Test {
     public static void main(String args[]) {
-        C obj = new C();
-        obj.print();
-        obj.show();
+        Status s = Status.Error;
+        int i = s.ordinal();
+        System.out.println(s + " " + i);
+
     }
 }
 
-interface A {
-    void show();
-}
-
-interface B extends A {
-    void print();
-}
-
-class C implements B {
-    public void show() {
-        System.out.println("This is show");
-    }
-
-    public void print() {
-        System.out.println("This is Print");
-    }
+enum Status {
+    Success, Pending, Error, Running
 }
