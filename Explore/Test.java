b1d5a8916d351e25b060ba395113e35a398fc5ed
Somnath Kar <somnathkar201901@gmail.com>
2025-01-23 22:00:13 +0530

    better way to read data from terminal

--- Explore/Test.java
+++ Explore/Test.java
@@ -1,19 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Test {
     public static void main(String args[]) throws IOException {
         System.out.print("Enter a Number ");
         // int num = System.in.read();
 
-        InputStreamReader in = new InputStreamReader(System.in);
-
-        BufferedReader bf = new BufferedReader(in);
-
-        int num = Integer.parseInt(bf.readLine());
-
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
         System.out.println("The number is " + num);
+
+        sc.close();
 
     }
 }
