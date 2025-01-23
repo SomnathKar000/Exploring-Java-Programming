838ca4b7d095d773f3342c8765b56ecf87883304
Somnath Kar <somnathkar201901@gmail.com>
2025-01-23 21:18:35 +0530

    my custom exception

--- Explore/Test.java
+++ Explore/Test.java
@@ -3,9 +3,18 @@ public class Test {
         int i = 0;
         int j = 0;
         try {
-            i = 18 / j;
+            i = 18 / 2;
+            throw new MyException("This one is created by me");
+        } catch (MyException err) {
+            System.out.println(err.toString());
         } catch (Exception err) {
             System.out.println("Error occurred " + err.getLocalizedMessage());
         }
+    }
+}
+
+class MyException extends Exception {
+    public MyException(String str) {
+        super(str);
     }
 }
