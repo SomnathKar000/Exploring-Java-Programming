1ef87e2cd6b7a95808faa7280703e8aa85409ade
Somnath Kar <somnathkar201901@gmail.com>
2025-01-27 22:24:19 +0530

    thread related issues

--- Concepts/Threads.java
+++ Concepts/Threads.java
@@ -2,68 +2,42 @@ package Concepts;
 
 public class Threads {
     public static void main(String args[]) {
+        Counter count = new Counter();
         Runnable obj1 = () -> {
-            try {
-                for (int i = 0; i < 5; i++) {
-                    System.out.println("Hi");
-                    Thread.sleep(1000);
-                }
-
-            } catch (Exception e) {
-                System.out.println(e.toString());
+            for (int i = 0; i < 10000; i++) {
+                count.increment();
             }
 
         };
         Runnable obj2 = () -> {
-            try {
-                for (int i = 0; i < 5; i++) {
-                    System.out.println("Hello");
-                    Thread.sleep(1000);
-                }
-            } catch (Exception e) {
-                System.out.println(e.toString());
+            for (int i = 0; i < 10000; i++) {
+                count.increment();
             }
+
         };
 
-        Thread t1 = new Thread(obj1);
-        Thread t2 = new Thread(obj2);
+        try {
+            Thread t1 = new Thread(obj1);
+            Thread t2 = new Thread(obj2);
 
-        t1.start();
-        t2.start();
+            t1.start();
+            t2.start();
+
+            t1.join();
+            t2.join();
+
+            System.out.println(count.num);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
 
     }
 }
 
-class A implements Runnable {
-    public void run() {
-        try {
-            show();
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-    }
+class Counter {
+    int num;
 
-    public void show() throws Exception {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Hi");
-            Thread.sleep(1000);
-        }
-    }
-}
-
-class B implements Runnable {
-    public void run() {
-        try {
-            show();
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-    }
-
-    public void show() throws Exception {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Hello");
-            Thread.sleep(1000);
-        }
+    public synchronized void increment() {
+        num++;
     }
 }
