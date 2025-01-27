00120e5efa7da7619ed96b3b9fd5137d9e9090d9
Somnath Kar <somnathkar201901@gmail.com>
2025-01-27 22:10:18 +0530

    Runnable with Threads

--- Concepts/Threads.java
+++ Concepts/Threads.java
@@ -2,8 +2,28 @@ package Concepts;
 
 public class Threads {
     public static void main(String args[]) {
-        Runnable obj1 = new A();
-        Runnable obj2 = new B();
+        Runnable obj1 = () -> {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("Hi");
+                    Thread.sleep(1000);
+                }
+
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            }
+
+        };
+        Runnable obj2 = () -> {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("Hello");
+                    Thread.sleep(1000);
+                }
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            }
+        };
 
         Thread t1 = new Thread(obj1);
         Thread t2 = new Thread(obj2);
