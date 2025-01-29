ad3a12587051d0a3b1fd96a1c8a846161dbbc5e5
Somnath Kar <somnathkar201901@gmail.com>
2025-01-29 22:05:29 +0530

    Set in java

--- Concepts/CollectionClasses.java
+++ Concepts/CollectionClasses.java
@@ -1,20 +1,16 @@
 package Concepts;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CollectionClasses {
     public static void main(String args[]) {
-        List<Integer> numbers = new ArrayList<Integer>();
-        numbers.add(78);
-        numbers.add(5);
-        numbers.add(6);
-        numbers.add(5);
-
-        // for (int num : numbers) {
-        // System.out.println(num);
-        // }
-        System.out.println(numbers.indexOf(8));
+        HashMap<Integer> set = new HashSet<Integer>();
+        set.add(34);
+        set.add(78);
+        set.add(34);
+        System.out.println(set.contains(3));
     }
 }
