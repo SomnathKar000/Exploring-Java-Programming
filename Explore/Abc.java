8ee3dcc04a7b9a661aaadd57d90cebc829d9e3d3
Somnath Kar <somnathkar201901@gmail.com>
2025-02-03 21:35:57 +0530

    restricting inheritence of an class by using sealed

--- Explore/Abc.java
+++ Explore/Abc.java
@@ -1,13 +1,21 @@
-import java.util.ArrayList;
-
 public class Abc {
     public static void main(String[] args) {
-        var obj = new ArrayList<>();
-        obj.add(2);
-        obj.add("sas");
 
-        for (var num : obj) {
-            System.out.println(num);
-        }
     }
+}
+
+sealed class A permits B, C {
+
+}
+
+final class B extends A {
+
+}
+
+non-sealed class C extends A {
+
+}
+
+class D {
+
 }
